482a7c2d09b3935ae895d0d342c189118c99f1da
Pete Miller <miller.pete@gmail.com>
2020-09-14 14:54:25 -0700

    inject-to-document accepts strigified function

--- common/contentScript/inject-to-document.ts
+++ common/contentScript/inject-to-document.ts
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
-export default function injectToDocument (isolatedFn: Function, ...codeVars: any[]) {
+export default function injectToDocument (isolatedFn: Function | string, ...codeVars: any[]) {
   // Convert function to single-scope and stringify
   const codeToEval = `(
     ${isolatedFn.toString()}
