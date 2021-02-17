ea036c37d844afeaa4f14135ef3a2ff878646f96
AmirSaber Sharifi <asharifi@brave.com>
2021-02-16 20:56:15 -0600

    extract orderID

--- scripts/brave_rewards/ads/conversions/yubico.ts
+++ scripts/brave_rewards/ads/conversions/yubico.ts
@@ -3,10 +3,18 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const initYubicoScript = () => {
-  const link = document.createElement('meta')
-  link.setAttribute('name', 'ad-conversion-id')
-  link.content = 'XXXX'
-  document.getElementsByTagName('head')[0].appendChild(link)
+  const el = document.getElementsByClassName('order-info')
+  let orderId = ''
+  if (el && el[0] && el[0].firstChild && el[0].firstChild.firstChild) {
+    orderId = el[0].firstChild.firstChild.textContent!.replace('Order Number: ', '')
+  }
+
+  if (orderId !== '') {
+    const link = document.createElement('meta')
+    link.setAttribute('name', 'ad-conversion-id')
+    link.content = orderId
+    document.getElementsByTagName('head')[0].appendChild(link)
+  }
 }
 
 initYubicoScript()
